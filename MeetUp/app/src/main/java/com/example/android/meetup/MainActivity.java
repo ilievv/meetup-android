479d5daf3de49f67ef15e21bc45bdf3d08d77e36
ilievv <dmxyyyy@gmail.com>
2017-01-21 13:14:31 +0200

    initial

---
+++ MeetUp/app/src/main/java/com/example/android/meetup/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.meetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+//        if (id == R.id)
+
+        return super.onOptionsItemSelected(item);
+    }
+}
