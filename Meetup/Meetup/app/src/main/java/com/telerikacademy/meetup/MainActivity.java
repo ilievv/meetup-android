f3e66d18bcf0595df16b470eb4e30cf3916a6b9b
ilievv <dmxyyyy@gmail.com>
2017-01-27 21:15:19 +0200

    toolbar added

--- Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/MainActivity.java
+++ Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/MainActivity.java
@@ -2,6 +2,8 @@ package com.telerikacademy.meetup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.tool_bar);
+        this.setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        menu.clear();
+        this.getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
     }
 }
