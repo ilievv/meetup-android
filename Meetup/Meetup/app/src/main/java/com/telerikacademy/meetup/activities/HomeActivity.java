169d69076d56b2b35d3ae49104e085aa6f000fcb
georgivelikov <gmvelikov@gmail.com>
2017-01-29 02:26:16 +0200

    HomeActivity started. Some location added.

---
+++ Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/activities/HomeActivity.java
@@ -0,0 +1,157 @@
+package com.telerikacademy.meetup.activities;
+
+import android.Manifest;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.telerikacademy.meetup.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class HomeActivity extends AppCompatActivity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private Toolbar toolbar;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        if (this.googleApiClient == null) {
+            this.buildGoogleApiClient();
+        }
+
+        this.locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)
+                .setFastestInterval(1 * 1000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return R.id.action_settings == id || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "onConnected", Toast.LENGTH_SHORT).show();
+        if(this.googleApiClient == null){
+            Log.v("", "");
+        }
+        if (checkPermission()) {
+            Location location = LocationServices.FusedLocationApi
+                    .getLastLocation(googleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(this.googleApiClient, locationRequest, this);
+            } else {
+                this.handleNewLocation(location);
+            }
+        }
+
+        GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this);
+
+        Log.v("Check", "In onConnected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("Connection failed: ", Integer.toString(connectionResult.getErrorCode()));
+        Log.e("Connection failed: ", connectionResult.getErrorMessage());
+
+        Toast.makeText(this, "onConnectionFailed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            this.googleApiClient.connect();
+        }
+
+        Log.v("Check", "In OnStart");
+    }
+
+    protected void onStop() {
+        this.googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        boolean res = result == 0;
+        return res;
+    }
+
+    private void handleNewLocation(Location location){
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = new ArrayList<>();
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String address = addresses.get(0).getAddressLine(0);
+
+        TextView longitudeTextView = (TextView) findViewById(R.id.address);
+        longitudeTextView.setText("Current address: " + address);
+
+    }
+}
