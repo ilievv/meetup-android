e720a0f12cfede12b369e109898ee3116adfdf83
ilievv <dmxyyyy@gmail.com>
2017-01-30 00:48:13 +0200

    line divider added

--- Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/activities/NearbyVenuesActivity.java
+++ Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/activities/NearbyVenuesActivity.java
@@ -2,6 +2,7 @@ package com.telerikacademy.meetup.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import com.telerikacademy.meetup.R;
 import com.telerikacademy.meetup.models.Venue;
 import com.telerikacademy.meetup.views.adapters.NearbyVenuesRecyclerAdapter;
+import com.telerikacademy.meetup.views.decorators.RecyclerViewDivider;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +39,8 @@ public class NearbyVenuesActivity extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         NearbyVenuesRecyclerAdapter recyclerAdapter = new NearbyVenuesRecyclerAdapter(venues);
 
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(),
+                linearLayoutManager.getOrientation()));
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(recyclerAdapter);
     }
