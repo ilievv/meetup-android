1262f2d6144297736ba94fbbb19c67913496c71c
ilievv <dmxyyyy@gmail.com>
2017-01-31 20:49:57 +0200

    back btn added

--- Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/fragments/ToolbarFragment.java
+++ Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/fragments/ToolbarFragment.java
@@ -4,16 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.MenuRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.*;
 
+import android.widget.Toast;
 import com.telerikacademy.meetup.R;
 import com.telerikacademy.meetup.interfaces.IMenuInflater;
 
 public class ToolbarFragment extends Fragment
         implements IMenuInflater {
 
+    private ActionBar actionBar;
     private AppCompatActivity currentActivity;
 
     public ToolbarFragment() {
@@ -34,6 +37,19 @@ public class ToolbarFragment extends Fragment
 
         Toolbar toolbar = (Toolbar) this.currentActivity.findViewById(R.id.tool_bar);
         this.currentActivity.setSupportActionBar(toolbar);
+        this.actionBar = this.currentActivity.getSupportActionBar();
+
+        if (this.actionBar != null) {
+            this.actionBar.setDisplayHomeAsUpEnabled(true);
+            this.actionBar.setDisplayShowHomeEnabled(true);
+
+            toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(currentActivity, "Back btn", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     public void inflateMenu(@MenuRes int menuRes, Menu menu, MenuInflater inflater) {
