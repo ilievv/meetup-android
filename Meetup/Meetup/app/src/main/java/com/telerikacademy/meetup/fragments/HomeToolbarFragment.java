8e396144c7360d67a4ee814d2e78b69e8b9be710
ilievv <dmxyyyy@gmail.com>
2017-02-02 21:30:50 +0200

    home toolbar added

---
+++ Meetup/Meetup/app/src/main/java/com/telerikacademy/meetup/fragments/HomeToolbarFragment.java
@@ -0,0 +1,22 @@
+package com.telerikacademy.meetup.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.telerikacademy.meetup.R;
+
+public class HomeToolbarFragment extends ToolbarFragment {
+
+    public HomeToolbarFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home_toolbar, container, false);
+    }
+}
