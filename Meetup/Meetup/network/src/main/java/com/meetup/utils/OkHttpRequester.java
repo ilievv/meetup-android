6b256966dfa98a4a097c6104fc344de8cfcea9b7
ilievv <dmxyyyy@gmail.com>
2017-02-11 19:26:57 +0200

    post added

--- Meetup/Meetup/network/src/main/java/com/meetup/utils/OkHttpRequester.java
+++ Meetup/Meetup/network/src/main/java/com/meetup/utils/OkHttpRequester.java
@@ -5,15 +5,15 @@ import com.meetup.utils.base.IResponse;
 import com.meetup.utils.base.IResponseFactory;
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 
 import javax.inject.Inject;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
 public class OkHttpRequester implements IRequester {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private final IResponseFactory responseFactory;
 
@@ -55,6 +55,63 @@ public class OkHttpRequester implements IRequester {
                 try {
                     Request.Builder requestBuilder = new Request.Builder()
                             .url(url);
+
+                    for (Map.Entry<String, String> pair : headers.entrySet()) {
+                        requestBuilder.addHeader(pair.getKey(), pair.getValue());
+                    }
+
+                    Request request = requestBuilder.build();
+
+                    Response response = httpClient.newCall(request).execute();
+
+                    IResponse responseParsed = responseFactory.createResponse(
+                            response.headers().toMultimap(), response.body().string(),
+                            response.message(), response.code());
+
+                    return Observable.just(responseParsed);
+                } catch (Exception e) {
+                    return Observable.error(e);
+                }
+            }
+        });
+    }
+
+    public Observable<IResponse> post(final String url, final String body) {
+        return Observable.defer(new Callable<ObservableSource<? extends IResponse>>() {
+            @Override
+            public ObservableSource<? extends IResponse> call() throws Exception {
+                try {
+                    RequestBody requestBody = RequestBody.create(JSON, body);
+
+                    Request request = new Request.Builder()
+                            .url(url)
+                            .post(requestBody)
+                            .build();
+
+                    Response response = httpClient.newCall(request).execute();
+
+                    IResponse responseParsed = responseFactory.createResponse(
+                            response.headers().toMultimap(), response.body().string(),
+                            response.message(), response.code());
+
+                    return Observable.just(responseParsed);
+                } catch (Exception e) {
+                    return Observable.error(e);
+                }
+            }
+        });
+    }
+
+    public Observable<IResponse> post(final String url, final String body, final Map<String, String> headers) {
+        return Observable.defer(new Callable<ObservableSource<? extends IResponse>>() {
+            @Override
+            public ObservableSource<? extends IResponse> call() throws Exception {
+                try {
+                    RequestBody requestBody = RequestBody.create(JSON, body);
+
+                    Request.Builder requestBuilder = new Request.Builder()
+                            .url(url)
+                            .post(requestBody);
 
                     for (Map.Entry<String, String> pair : headers.entrySet()) {
                         requestBuilder.addHeader(pair.getKey(), pair.getValue());
